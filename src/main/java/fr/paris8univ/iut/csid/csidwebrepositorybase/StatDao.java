9149c929151d70c6fa2830eb67f3d8dcd38ef9bd
redark6 <redark66@gmail.com>
2020-12-11 13:33:24 +0100

    fix desc to asc into psql request

--- src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/StatDao.java
+++ src/main/java/fr/paris8univ/iut/csid/csidwebrepositorybase/StatDao.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StatDao extends JpaRepository<StatEntity, Integer> {
 	
-	@Query(value = "select * from statistique as s where s.name= ?1 and s.entry_type= ?2 order by s.creationDate desc ",nativeQuery=true)
+	@Query(value = "select * from statistique as s where s.name= ?1 and s.entry_type= ?2 order by s.creationDate asc ",nativeQuery=true)
 	List<StatEntity> findAllStatForNameOrderByDate(String repository, String statType);
 
 }
